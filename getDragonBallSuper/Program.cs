dad74f52597f17564e7f426ad7bcc537798d2e8a
Manuele Borghin <manuele.borghin@outlook.com>
2023-03-05 13:51:19 +0100

    getting all img tag

--- getDragonBallSuper/Program.cs
+++ getDragonBallSuper/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using HtmlAgilityPack;
+using System.Net;
+using System.Text;
+
+string html;
+// obtain some arbitrary html....
+
+
+
+for (int volume = 1; volume <= 83; volume++)
+{
+    string url = $"https://planetadragonball.com/dragon-ball-super-manga-{volume}-espanol/";
+
+    using (var client = new WebClient())
+    {
+        html = client.DownloadString(url);
+    }
+
+    HtmlDocument doc = new HtmlDocument();
+    doc.LoadHtml(html);
+
+    HtmlNodeCollection figureOfImgs = doc.DocumentNode.SelectNodes("//figure[@class='wp-block-image']//img");
+
+
+}
